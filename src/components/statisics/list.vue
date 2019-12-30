212480ff0364cd19e965cea75e633b9027f0a469
YeZh <yezh9805@hotmail.com>
2019-12-30 19:58:08 +0800

    Signed-off-by: YeZh <yezh9805@hotmail.com>

--- src/components/statisics/list.vue
+++ src/components/statisics/list.vue
@@ -3,9 +3,9 @@
 		<el-table :data="tableData" border style="width: 100%">
 			<el-table-column prop="date" label="日期" width="180">
 			</el-table-column>
-			<el-table-column prop="name" label="姓名" width="180">
+			<el-table-column prop="name" label="用户" width="180">
 			</el-table-column>
-			<el-table-column prop="address" label="地址">
+			<el-table-column prop="address" label="文章">
 			</el-table-column>
 		</el-table>
 		<el-pagination small layout="prev, pager, next" :total="50">
@@ -19,20 +19,20 @@
 			return {
 				tableData: [{
 					date: '2016-05-02',
-					name: '王小虎',
-					address: '上海市普陀区金沙江路 1518 弄'
+					name: 'MrJ',
+					address: '面试还搞不懂redis，快看看这40道面试题'
 				}, {
 					date: '2016-05-04',
-					name: '王小虎',
-					address: '上海市普陀区金沙江路 1517 弄'
+					name: 'S_mengyong孟勇丶',
+					address: 'H5项目性能优化注意事项'
 				}, {
 					date: '2016-05-01',
-					name: '王小虎',
-					address: '上海市普陀区金沙江路 1519 弄'
+					name: '自信人生-会当击',
+					address: 'Spring boot框架相关知识详解'
 				}, {
 					date: '2016-05-03',
-					name: '王小虎',
-					address: '上海市普陀区金沙江路 1516 弄'
+					name: 'HTML小白呀',
+					address: 'vue+element-ui实现动态的权限管理和菜单渲染'
 				}]
 			}
 		}
